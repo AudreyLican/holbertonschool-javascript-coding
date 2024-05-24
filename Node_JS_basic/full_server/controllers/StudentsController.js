5347d5e9fd4e61c807542d67c3a55c946a57787a
AudreyLican <audrey.lican@gmail.com>
2024-05-24 22:04:44 +0200

    added readme and folder full_server

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,60 @@
+import { readDatabase } from '../utils';
+
+class StudentsController {
+    static async getAllStudents(req, res) {
+        try {
+            const databaseFile = process.argv[2] || '';
+            const students = await readDatabase(databaseFile);
+
+            if (!students || students.length === 0) {
+                return res.status(500).send('Failed to load the database');
+            }
+
+            const majorCounts = new Map();
+
+            students.CS.forEach(() => {
+                majorCounts.set('CS', (majorCounts.get('CS') || 0) + 1);
+            });
+
+            students.SWE.forEach(() => {
+                majorCounts.set('SWE', (majorCounts.get('SWE') || 0) + 1);
+            });
+
+            let responseContent = 'List of students by major\n';
+
+            majorCounts.forEach((count, major) => {
+                const firstNames = students[major].join(', ');
+                responseContent += `Students in ${major}: ${count}. Names: ${firstNames}\n`;
+            });
+
+            return res.status(200).send(responseContent);
+        } catch (error) {
+            return res.status(500).send('Failed to load the database');
+        }
+    }
+
+    static async getAllStudentsByMajor(req, res) {
+        try {
+            const { major } = req.params;
+
+            if (major !== 'CS' && major !== 'SWE') {
+                return res.status(500).send('Major parameter must be CS or SWE');
+            }
+
+            const databaseFile = process.argv[2] || '';
+            const students = await readDatabase(databaseFile);
+
+            if (!students || students.length === 0) {
+                return res.status(500).send('Failed to load the database');
+            }
+
+            const firstNames = students[major].join(', ');
+            return res.status(200).send(`List: ${firstNames}`);
+        } catch (error) {
+            return res.status(500).send('Failed to load the database');
+        }
+    }
+}
+
+module.exports = StudentsController;
+export default StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+async function readDatabase(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error('Cannot load the database'));
+            } else {
+                const lines = data.trim().split('\n');
+                const headers = lines[0].split(',');
+                const students = { CS: [], SWE: [] };
+
+                for (let i = 1; i < lines.length; i += 1) {
+                    const values = lines[i].split(',');
+
+                    const student = {};
+                    for (let j = 0; j < headers.length; j += 1) {
+                        student[headers[j]] = values[j];
+                    }
+
+                    students[student.field.toUpperCase()].push(student.firstname);
+                }
+
+                resolve(students);
+            }
+        });
+    });
+}
+
+module.exports = { readDatabase };
